92af9c1b254f08001bfb1f7c526cc7d946372f0a
tangli <tangli@kuaishou.com>
2021-09-27 13:43:38 +0800

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue';
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Test4" />
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view></router-view>
 </template>
 
 <style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+  background: #000;
+  color: #fff;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/pages/About.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>It's a site to the crypto world.</div>
+  <div>Enjoy.</div>
+</template>
